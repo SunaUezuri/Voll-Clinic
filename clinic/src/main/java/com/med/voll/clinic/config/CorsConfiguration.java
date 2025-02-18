cffa9ded2aa5aa5ca12c840bbdc6ad0a2ffc18b8
SunaUezuri <wesley.jane.santos@gmail.com>
2025-02-18 20:11:52 -0300

    Creating a CorsConfiguration class to avoid the CORS Error.

---
+++ clinic/src/main/java/com/med/voll/clinic/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.med.voll.clinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Annotation to tell Spring that this class is a configuration class
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}

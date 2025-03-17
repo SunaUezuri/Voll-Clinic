0f32c06c76af12fd74d8cd8105d09e34ddca5ff9
SunaUezuri <wesley.jane.santos@gmail.com>
2025-03-17 20:47:37 -0300

    Starting the class mapping for the class Doctor

--- clinic/src/main/java/com/med/voll/clinic/model/Doctor.java
+++ clinic/src/main/java/com/med/voll/clinic/model/Doctor.java
@@ -1,18 +1,29 @@
 package com.med.voll.clinic.model;
 
 import com.med.voll.clinic.model.enums.Specialty;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 //Using annotatios for better perfomance and practicality
+/*
+* Loombok annotation gives a way to reduce
+* the amount of repetitive code, giving more practicality
+* and less time coding repetitive methods
+* such as getters, setters and constructors
+*/
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "T_VOLL_DOCTOR")
 public class Doctor {
 
     //Class attributes
+    @Id
+    private Long id;
     private String name;
     private String email;
     private String crm;
